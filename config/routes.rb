91d18d80fdbfed54ce7d0a93338c024115c5e6b4
SuperBigBang <superbigbang@yandex.ru>
2019-05-05 19:49:41 +0900

    feat: api now correctly work on no local connection

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'main_welcome/index'
+  #get 'main_welcome/index'
   root 'main_welcome#index'
 
-  resources :main_welcome
+  namespace :api, constraints: {format: 'json'} do
+    namespace :v1 do
+      resources :main_welcome, :defaults => {:format => :json}
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
